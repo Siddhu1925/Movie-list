fcf5a3ac831210c87da0d27bd3d39063b5a8cd28
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-03-18 14:47:10 +0530

    Update GenreFilter.js

--- src/GenreFilter.js
+++ src/GenreFilter.js
@@ -2,6 +2,7 @@ import React from 'react';
 function GenreFilter({ genres, onGenreSelect }) {
   return (
     <div className="genre-filter">
+    <br/>
       <h2 className="App-title1">Filter by Genre</h2>
       {genres.map((genre, index) => (
         <button key={index} onClick={() => onGenreSelect(genre)}>
