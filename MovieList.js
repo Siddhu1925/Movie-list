20c4eac7661919eb15c66892730a3b3dfa77ed6e
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-03-18 09:20:22 +0530

    Initialize project using Create React App

---
+++ MovieList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function MovieList(props) {
+  const { movies } = props;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie, index) => (
+          <tr key={index}>
+            <td>{movie.title}</td>
+            <td>{movie.genre}</td>
+            <td>{movie.year}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default MovieList;
+
