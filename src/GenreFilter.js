dce01b5d74cee0524ec0d3d9fc54be3c2507bfe2
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-03-18 12:01:15 +0530

    Add files via upload

---
+++ src/GenreFilter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+function GenreFilter({ genres, onGenreSelect }) {
+  return (
+    <div className="genre-filter">
+      <h2 className="App-title1">Filter by Genre</h2>
+      {genres.map((genre, index) => (
+        <button key={index} onClick={() => onGenreSelect(genre)}>
+          {genre}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default GenreFilter;
+
+
+
+
